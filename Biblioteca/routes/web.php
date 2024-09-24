3e338d7fbc83064f38b93834452aa190201bfa31
VictorLemos1000 <vitinhotoinho@gmail.com>
2024-09-24 00:29:35 -0300

    Criando views, autenticação, rotas e layouts.

--- Biblioteca/routes/web.php
+++ Biblioteca/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('books', BookController::class);
+
+Route::resource('authors', AuthorController::class);
+
+Route::resource('categories', CategoryController::class);
+
+Route::resource('publishers', PublisherController::class);
